00890b4d7a67a325993cfc13f8cdf09d147f0877
S-BerlinNew <150stevlin@gmail.com>
2025-07-08 08:31:36 +0700

    test

---
+++ lib/service/database.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  Future addEmployeeDetails(
+    Map<String, dynamic> employeeInfoMap,
+    String id,
+  ) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .set(employeeInfoMap);
+  }
+
+  Future<Stream<QuerySnapshot>> getEmployeeDetails() async {
+    return await FirebaseFirestore.instance.collection("Employee").snapshots();
+  }
+
+  Future updateEmployeeDetail(
+    String id,
+    Map<String, dynamic> updateInfo,
+  ) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .update(updateInfo);
+  }
+
+    Future deleteEmployeeDetails(
+    String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .delete();
+  }
+}
